a8c8e157d133372c59f4d39bf7ee3eb530710f15
Jagad Wijaya Purnomo <Jagadpurnomo26@gmail.com>
2021-10-29 16:11:11 +0700

    db: change to pgsql

--- databaseconnect.php
+++ databaseconnect.php
@@ -1,13 +1,13 @@
 <?php
 
-$servername = "localhost";
-$database = "studi-kasus-1";
-$username = "root";
-$password = "";
+$servername = "ec2-34-200-161-87.compute-1.amazonaws.com";
+$database = "d2aqfsffi4v3u6";
+$username = "ohdlnkifwayxrp";
+$password = "c12db400cbb30d8c8cb0aa445da7a1ed7e8c43a73f0ae669a841132c6d232e92";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// $conn = pg_connect("host=$servername dbname=$database user=$username password=$password");
+$conn = pg_connect("host=$servername dbname=$database user=$username password=$password");
+// $conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
 if (!$conn) {
